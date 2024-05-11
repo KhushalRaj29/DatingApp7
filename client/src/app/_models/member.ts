eaa1e6fb7e260044556e7e943e723134231a3b1f
KhushalRaj29 <khushalraj1229@gmail.com>
2024-05-11 10:05:04 +0530

    End of section 10

--- client/src/app/_models/member.ts
+++ client/src/app/_models/member.ts
@@ -9,7 +9,7 @@ export interface Member{
     created:Date;
     lastActive:Date;
     gender:string;
-    introduction:string;
+    itroduction:string;
     lookingFor:string;
     interests:string;
     city:string;
